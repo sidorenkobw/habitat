69a0fe7390051482039104c90f4a5906bd8f9f07
Andrey Kupreychik <kupreychik_af@bitworks-software.com>
2014-03-06 12:22:33 +0700

    Foxel agent removed debug

--- srv/agents/Foxel.js
+++ srv/agents/Foxel.js
@@ -443,7 +443,7 @@ var FoxelAgent = createClass({
         }, this);
 
 //        console.log(this.status);
-        console.log(this.getMapString());
+//        console.log(this.getMapString());
     },
 
     'decision': function () {
